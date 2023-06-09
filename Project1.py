3711afe1315b74076a3b1d755b2b4b27838ae7e4
Prabin <prabinbhattarai43@gmail.com>
2023-06-09 11:06:52 +0545

    New PY Project

---
+++ Project1.py
@@ -0,0 +1,23 @@
+# Using Python language solve the below problem.
+# 1. Area of Circumference of  the  circle
+
+
+# pi = 3.14
+# radius = int (input ('Enter radius of the circle:'))
+# circumference = 2*pi*radius
+# area = pi *radius*radius
+# print ("Area of the circle: ",area)
+# print ("Circumference of the circle: ",  circumference )
+
+# # 2. Print ASCI Vale of Characters
+# character = input ('Enter character to print : ')
+# print ("The ASCII Vale of Characters: ", ord(character))
+
+# Area of Triangle
+import math
+print ("Area of Triangle ABC")
+a = int ( input("Enter the area of AB: "))
+b = int ( input("Enter the area of BC: "))
+c = int ( input("Enter the area of CA: "))
+s=(a+b+c)/2
+print ("Area of Triangle", s)
